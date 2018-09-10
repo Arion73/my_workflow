a17bb667dcf9d5838013454cb1c3ff285fb208ba
arion73 <24527962+Arion73@users.noreply.github.com>
2018-09-10 19:17:42 +0200

    add c layer

---
+++ .emacs.d/core/core-c-c++.el
@@ -0,0 +1,17 @@
+
+;;; core-c-c++.el --- configuration for the C/C++ layer of my emacs.
+
+;; Author: Zewei Wang
+
+;; This file is not part of GNU Emacs.
+
+;;; Commentary:
+
+;; Provide C layer configurations.
+
+;;; Code:
+
+(add-to-list 'load-path "~/.emacs.d/private/disaster-master")
+(require 'disaster)
+
+(provide 'core-c-c++)
