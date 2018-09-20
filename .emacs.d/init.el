f82d740cf6620dc2fe82c53481b00d8bf36789f8
arion73 <24527962+Arion73@users.noreply.github.com>
2018-09-21 00:29:29 +0200

    update init.el make emacs startup faster

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,15 +1,36 @@
 ;;; package --- Summary
 
-;;; Author : Zewei Wang
+;;; Author : Z.Wang
 ;;; Year : 2018
 
 
 ;;; Commentary:
 
-;;; This is a private configuration for my Emacs.
+;;; This is not a part of GNU Emacs.
+;;; Instead, it is a private configuration file for my own Emacs.
 
 ;;; Code:
+
+
+(let ((file-name-handler-alist nil))
+
+;;==========================================================
+  
+;; Make startup faster by redcing the frequency of garbage collection.
+;; The default is 800000 bytes.
+(setq gc-cons-threshold 30000000)
+
+;;==========================================================
+
 (require 'package)
+(setq package-enable-at-startup nil)
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
 ;; add melpa stable
 (add-to-list 'package-archives
              '("melpa-stable" . "https://stable.melpa.org/packages/"))
@@ -17,99 +38,98 @@
 (add-to-list 'package-archives
              '("melpa" . "http://melpa.milkbox.net/packages/"))
 
-;; Added by Package.el.  This must come before configurations of
-;; installed packages.  Don't delete this line.  If you don't want it,
-;; just comment it out by adding a semicolon to the start of the line.
-;; You may delete these explanatory comments.
-(package-initialize)
-;;;-----------------------------------------------------------------
-;; my configurations
-;;;-----------------------------------------------------------------
-
 (when (not package-archive-contents)
     (package-refresh-contents))
 
-(defvar my-packages
-  '(
-    ;; better defaults
-    yasnippet-snippets
-    ;; python layer
-    elpy
-    flycheck  ;; realtime syntax checking
-    py-autopep8
-    ))
+;;==========================================================
+;; MY CONFIGURATIONS
+;;==========================================================
 
-(mapc #'(lambda (package)
-	  (unless (package-installed-p package)
-	    (package-install package)))
-      my-packages)
-
-;; directory of all my configuration files
+;; directory of my core configuration files
 (add-to-list 'load-path "~/.emacs.d/core/")
 
 ;;==========================================================
-;; evil
+;; PACKAGE MANAGEMENT
+;; Note: Keep this block near the top, because it needs to
+;; count loaded packages number and time spent.
 ;;==========================================================
+
+(require 'core-package-management)
+
+;;==========================================================
+;; BASIC CONFIGURATIONS
+;;==========================================================
+
+(require 'core-basic)
+
+;;==========================================================
+;; EVIL
+;;==========================================================
+
 (require 'core-evil)
 
 ;;==========================================================
-;; better defaults
+;; PYTHON
 ;;==========================================================
-(savehist-mode 1)
-(require 'core-better-defaults)
 
-;; yasnippet
-(setq yas-snippet-dirs
-	'("~/.spacemacs.d/private/snippets"      ;; personal snippets
-	"~/.spacemacs.d/elpa/yasnippet-snippets-20180714.1322/snippets"))
+(require 'core-python)
+
 ;;==========================================================
-;; theme
+;; C/C++  
 ;;==========================================================
+
+(require 'core-c-c++)
+
+;;==========================================================
+;; MARKDOWN
+;;==========================================================
+
+(require 'core-markdown)
+
+;;==========================================================
+;; AUTO-COMPLETION
+;;==========================================================
+
+(require 'core-auto-completion)
+
+;; flycheck
+(use-package flycheck
+  :ensure t
+  :config
+  (add-hook 'elpy-mode-hook 'flycheck-mode)
+  )
+
+;;==========================================================
+;; KEY BINDINGS
+;;==========================================================
+
+(require 'core-key-bindings)
+
+;;==========================================================
+;; THEME & MODE LINE
+;;==========================================================
+
+;; load my own theme path
 (add-to-list 'custom-theme-load-path "~/.emacs.d/private/zelin-theme")
+
 (setq ns-use-srgb-colorspace nil)
 
+;; load theme for gui and terminal respectivelay
 (if (display-graphic-p)
     (progn
       ;; if graphic
-      (load-theme 'zelin-dark-02-gui))
+      (load-theme 'zelin-dark-02-gui t))
   ;; else
-  (load-theme 'zelin-dark-02-terminal))
-;;-----------------------------------------------------------
+  (load-theme 'zelin-dark-02-terminal t))
+
 ;; mode line
-;;-----------------------------------------------------------
 (require 'core-mode-line)
 
-;;-----------------------------------------------------------
-;; key-bindings
-;;-----------------------------------------------------------
-(require 'core-key-bindings)
-
-;;-----------------------------------------------------------
-;; python
-;;-----------------------------------------------------------
-(require 'core-python)
-
-;; elpy
-(elpy-enable)
-;; highlight-indentation is a dependency package. Do not display highlight-indentation as default
-(add-hook 'elpy-mode-hook (lambda() (highlight-indentation-mode -1)))
-
-;; flycheck
-(when (require 'flycheck nil t)
-  (setq elpy-modules (delq 'elpy-module-flymake elpy-modules))
-  (add-hook 'elpy-mode-hook 'flycheck-mode))
-
-;; py-autopep8
-(add-hook 'elpy-mode-hook 'py-autopep8-enable-on-save)
-
-;;-----------------------------------------------------------
-;; markdown 
-;;-----------------------------------------------------------
-(require 'core-markdown)
-
-;;-----------------------------------------------------
-;;; package.el produced configuration automatically.
-;;-----------------------------------------------------
+;;==========================================================
+)  ;; let ends here
+;;==========================================================
+;; PACKAGE.EL AUTOMATICALLY PRODUCED CONFIGURATIONS
+;;==========================================================
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
@@ -121,7 +141,7 @@
     ("04bff26d45b0ac4e64295eb5c73585e051a7d12cea771e54bb234d94a2ff59f8" "7b839cbaaf0f7da876cab50d745e65ee6a07a5c03edc8ae90defd33ab1af88ce" default)))
  '(package-selected-packages
    (quote
-    (pandoc-mode company-auctex auctex-latexmk ein ob-ipython org-projectile org-download org-present org-bullets markdown-preview-mode markdown-mode nose anaconda-mode virtualenvwrapper key-chord general window-numbering company-statistics auto-complete rainbow-delimiters which-key smex counsel osx-clipboard evil-matchit evil-indent-textobject evil-surround evil-leader evil yasnippet-snippets use-package py-autopep8 flycheck elpy))))
+    (esup exec-path-from-shell pandoc-mode company-auctex auctex-latexmk ein ob-ipython org-projectile org-download org-present org-bullets markdown-preview-mode markdown-mode nose anaconda-mode virtualenvwrapper key-chord general window-numbering company-statistics auto-complete rainbow-delimiters which-key smex counsel osx-clipboard evil-matchit evil-indent-textobject evil-surround evil-leader evil yasnippet-snippets use-package py-autopep8 flycheck elpy))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -130,5 +150,8 @@
  )
 
 
+;;==========================================================
+;; Make gc pauses faster by setting back the default value.
+(setq gc-cons-threshold 800000)
 (provide 'init)
 ;;; init.el ends here
