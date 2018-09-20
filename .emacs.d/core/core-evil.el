f0018dbcf010f65308d5a108b4a1bd6c5e9b8706
arion73 <24527962+Arion73@users.noreply.github.com>
2018-09-21 00:34:10 +0200

    update core-evil.el: add defer

--- .emacs.d/core/core-evil.el
+++ .emacs.d/core/core-evil.el
@@ -1,40 +1,70 @@
-;;; core-evil.el
+;;; core-evil.el --- Configurations for Evil mode.
 
-;; Author: Zewei Wang
+;; Author: Z.Wang
 
 ;; This file is not part of GNU Emacs.
 
-;; Commentary:
+;;; Commentary:
 
 ;; Configurations for Evil mode.
 
-;; Code:
+;;; Code:
 
 ;; evil-mode
 (use-package evil
   :ensure t
+  :defer t
   :config
-  (evil-mode 1)
+  (evil-mode 1))
 
-  ;; more configuration goes here
-  (use-package evil-leader
-    :ensure t
-    :config
-    (global-evil-leader-mode))
+;; key-chord
+(use-package key-chord
+  :ensure t
+  :after evil
+  :diminish ""
+  :config
+  ;; Max time delay between two key presses to be considered a key chord
+  (setq key-chord-two-keys-delay 0.1)
+  (key-chord-define evil-insert-state-map "ii" 'evil-normal-state)
+  (key-chord-define evil-replace-state-map "ii" 'evil-normal-state)
+  (key-chord-define evil-motion-state-map "ii" 'evil-normal-state)
+  (key-chord-mode t))
 
-  (use-package evil-surround
-    :ensure t
-    :config
-    (global-evil-surround-mode))
 
-  (use-package evil-indent-textobject
-    :ensure t))
+(use-package evil-surround
+  :ensure t
+  :hook (evil-mode-hook)
+  :after (evil)
+  :config
+  (global-evil-surround-mode))
+
+(use-package evil-indent-textobject
+  :ensure t
+  :hook (evil-mode-hook)
+  :after (evil))
 
 
 ;; evil-matchit --- jump between blocks
 (use-package evil-matchit
   :ensure t
+  :hook (evil-mode-hook)
+  :after (evil)
   :config
   (global-evil-matchit-mode 1))
 
+
+;; apply the Evil h,j,k,l bindings to occur-mode-map when Emacs state
+(add-hook 'occur-mode-hook
+	        (lambda ()
+	          (evil-add-hjkl-bindings occur-mode-map 'emacs
+		          (kbd "/") 'evil-search-forward
+		          (kbd "n") 'evil-search-next
+		          (kbd "N") 'evil-search-previous
+		          (kbd "C-f") 'evil-scroll-down
+		          (kbd "C-b") 'evil-scroll-up
+		          (kdb "C-w C-w") 'other-window)))
+
+
+
 (provide 'core-evil)
+;;; core-evil.el ends here
