e41153d1f716cc9cd3e219a8cc826a71e378dd83
arion73 <24527962+Arion73@users.noreply.github.com>
2018-09-21 00:30:58 +0200

    update mode-line: add project name

--- .emacs.d/core/core-mode-line.el
+++ .emacs.d/core/core-mode-line.el
@@ -1,6 +1,6 @@
 ;;; core-mode-line.el --- configuration for the mode-line of my emacs.
 
-;; Author: Zewei Wang
+;; Author: Z.Wang
 
 ;; This file is not part of GNU Emacs.
 
@@ -11,6 +11,12 @@
 ;; my own "ugly-simple-powerful" mode-line as following.
 
 ;;; Code:
+
+(defadvice vc-mode-line (after strip-backend () activate)
+  (when (stringp vc-mode)
+    (let ((gitlogo (replace-regexp-in-string "^ Git." ":" vc-mode)))
+          (setq vc-mode gitlogo))))
+
 
 (setq-default ns-use-srgb-colorspace nil)
 
@@ -48,29 +54,25 @@
   "evil-visual color"
   :group 'faces)
 
-(defface my-repace-face
+(defface my-replace-face
   '((t (:foreground "#333333" :background "#F11712")))
   "evil-replace color"
   :group 'faces)
 
 (defface my-operator-face
-  '((t (:foreground "#333333" :background "#1019DE")))
+  '((t (:foreground "#FFFFFF" :background "#1019DE")))
   "evil-operator color"
   :group 'faces)
 
 (defface my-motions-face
-  '((t (:foreground "#333333" :background "#1D9FFF")))
+  '((t (:foreground "#FFFFFF" :background "#1D9FFF")))
   "evil-operator color"
   :group 'faces)
 (defface my-emacs-face
-  '((t (:foreground "#333333" :background "#B612B8" )))
+  '((t (:foreground "#FFFFFF" :background "#B612B8" )))
   "evil-emacs color"
   :group 'faces)
 
-(defface my-highlight-face
-  '((t (:foreground "#FFC820" :background "black")))
-  "my-red-face"
-  :group 'face)
 
 ;; evil tag color
 (setq evil-normal-state-tag
@@ -95,50 +97,50 @@
     (format (format "%%s %%%ds " available-width) left right)))
 
 (defun my-mode-line ()
-;; my mode-line-format
-(setq-default mode-line-format
-	      (list
-	       '(:eval (mode-line-render
-		      ;;left
-		      (format-mode-line
-		       (list 
-			'(:eval (propertize (concat " " (window-numbering-get-number-string) " ") 'face 'my-normal-face))
-			;; evil tag
-			evil-mode-line-tag
-			" "
-			;; encoding system
-			mode-line-mule-info
-			;; buffer modified
-			mode-line-modified
-			;; buffer read only
-			mode-line-client
-			mode-line-remote
-			;; buffer size
-			" %I "
-			;; buffer name
-			'(:eval (propertize " %b " 'face 'my-normal-face))
-			;; value of "mode-name"
-			'(:eval (propertize " %m "))
-			;; flycheck
-			(flycheck-mode-line-status-text)
-			" "
-			;; git info
-		        vc-mode
-			" "
-			;; pyvenv-virtual-env-name
-			'(:eval (when (> (length pyvenv-virtual-env-name) 3)
-				  (propertize (concat " " pyvenv-virtual-env-name " ") 'face 'my-normal-face)))
-			 ))
-		      ;; right
-		      (format-mode-line
-		       (list
-			 ;; line number, column number and percent of buffer above bottom of window
-			" %02l:%02c | %p%%  "
-			mode-line-misc-info
-			 )))))))
+    ;; my mode-line-format
+    (setq-default mode-line-format
+		(list
+		'(:eval (mode-line-render
+			;;left
+			(format-mode-line
+			(list 
+			    '(:eval (propertize (concat " " (window-numbering-get-number-string) " ") 'face 'my-normal-face))
+			    ;; evil tag
+			    evil-mode-line-tag
+			    " "
+			    ;; encoding system
+			    mode-line-mule-info
+			    ;; buffer modified
+			    mode-line-modified
+			    ;; buffer read only
+			    mode-line-client
+			    mode-line-remote
+			    ;; buffer size
+			    " %I "
+			    ;; buffer name
+			    '(:eval (propertize " %b " 'face 'my-normal-face))
+			    ;; value of "mode-name"
+			    '(:eval (propertize " %m "))
+			    ;; project name
+			    '(:eval (if projectile-mode (when (ignore-errors (projectile-project-root)) (format " Proj[%s] " (projectile-project-name)))))
+			    ;; flycheck
+			    (flycheck-mode-line-status-text)
+			    " "
+			    ;; pyvenv-virtual-env-name
+			    '(:eval (if pyvenv-mode (when (stringp pyvenv-virtual-env-name)
+				      (propertize (concat " " pyvenv-virtual-env-name " ") 'face 'my-normal-face))))
+			    ))
+			;; right
+			(format-mode-line
+			(list
+			    ;; git info
+			    vc-mode
+			    ;; line number, column number and percent of buffer above bottom of window
+			    "  %02l:%02c | %p%%  "
+			    mode-line-misc-info
+			    )))))))
 
 (my-mode-line)
 
 (provide 'core-mode-line)
-
-
+;;; core-mode-line.el ends here
