05bcf9dcdf355ebc5dea44b6ba8407e8542d5e3e
arion73 <24527962+Arion73@users.noreply.github.com>
2018-09-30 02:27:19 +0200

    add core-org

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -23,29 +23,26 @@
 (add-hook 'after-init-hook #'(lambda()
 			       (setq gc-cons-threshold 800000)))
 
-;;==========================================================
-;; Added by Package.el.  This must come before configurations of
-;; installed packages.  Don't delete this line.  If you don't want it,
-;; just comment it out by adding a semicolon to the start of the line.
-;; You may delete these explanatory comments.
-(package-initialize)
-;;==========================================================
-
 ;; directory of my core configuration files
 (add-to-list 'load-path "~/.emacs.d/core/")
-;; Note: Keep core-package near the top, as it needs to
-;; count loaded packages number and time spent.
+;; Note: Keep core-package at the top
 (require 'core-package)          ;; package management layer
 (require 'core-basic)            ;; basic config 
 (require 'core-evil)             ;; evil layer
+(require 'core-theme)            ;; load theme
+
 (require 'core-python)           ;; python layer
 (require 'core-c-c++)            ;; C/C++ layer
+
+(require 'core-org)              ;; org layer
 (require 'core-markdown)         ;; markdown layer
+(require 'core-latex)            ;; latex layer
+(require 'core-pdf)              ;; pdf layer
+
 (require 'core-auto-completion)  ;; completion layer
 (require 'core-global)           ;; package collections for global mode 
 (require 'core-key-bindings)     ;; key-bindings
 (require 'core-mode-line)        ;; mode-line
-(require 'core-theme)            ;; load theme
 )  ;; let ends here
 ;;
 ;;==========================================================
@@ -62,7 +59,7 @@
     ("04bff26d45b0ac4e64295eb5c73585e051a7d12cea771e54bb234d94a2ff59f8" "7b839cbaaf0f7da876cab50d745e65ee6a07a5c03edc8ae90defd33ab1af88ce" default)))
  '(package-selected-packages
    (quote
-    (evil-lion highlight-parentheses diff-hl esup exec-path-from-shell pandoc-mode company-auctex auctex-latexmk ein ob-ipython org-projectile org-download org-present org-bullets markdown-preview-mode markdown-mode nose anaconda-mode virtualenvwrapper key-chord general window-numbering company-statistics auto-complete rainbow-delimiters which-key smex counsel osx-clipboard evil-matchit evil-indent-textobject evil-surround evil-leader evil yasnippet-snippets use-package py-autopep8 flycheck elpy))))
+    (htmlize h2o all-the-icon evil-lion highlight-parentheses diff-hl esup exec-path-from-shell pandoc-mode company-auctex auctex-latexmk ob-ipython org-projectile org-download org-present org-bullets markdown-preview-mode markdown-mode nose anaconda-mode virtualenvwrapper key-chord general window-numbering company-statistics auto-complete rainbow-delimiters which-key smex counsel osx-clipboard evil-matchit evil-indent-textobject evil-surround evil-leader evil yasnippet-snippets use-package py-autopep8 flycheck elpy))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
