b7fb3a238d9b7beefd7ff0a1bcd7733a9036985a
arion73 <24527962+Arion73@users.noreply.github.com>
2018-11-12 23:04:51 +0100

    collect all MacOSX configuration in one file

---
+++ .emacs.d/core/core-osx.el
@@ -0,0 +1,45 @@
+;;; core-osx.el --- Specific configurations for MacOSX.
+
+;; Author: Z.Wang
+
+;; This file is not part of GNU Emacs.
+
+;;; Commentary:
+
+;; Specific configurations for MacOSX.
+
+;;; Code:
+
+
+(add-hook 'after-init-hook
+	  (lambda ()
+
+	    (setq-default ns-use-srgb-colorspace nil)
+
+	    ;; In my env case (mac OSX, terminal), <Backspace> fails to delete. Below solves this problem.
+	    (normal-erase-is-backspace-mode 0)))
+
+
+;; dired-use-ls-dired problem fix
+(setq dired-use-ls-dired nil)
+
+
+;; use OS X clipboard from terminal emacs
+(use-package osx-clipboard
+  :defer t
+  :diminish ""
+  :config
+  (osx-clipboard-mode t))
+
+
+;; ensure environment variables inside Emacs look the same in the shell
+(use-package exec-path-from-shell
+  :if (eq system-type 'darwin)
+  :defer t
+  :config
+  (exec-path-from-shell-initialize))
+
+
+
+(provide 'core-osx)
+;; core-osx.el ends here
