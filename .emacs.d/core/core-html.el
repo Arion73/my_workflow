7594bfd9668d46d0b78ace6aa7e055e07625c835
Arion73 <24527962+Arion73@users.noreply.github.com>
2019-04-05 11:29:28 +0200

    add web-mode

--- .emacs.d/core/core-html.el
+++ .emacs.d/core/core-html.el
@@ -13,15 +13,42 @@
 
 ;; htmlize --- syntax highlighting in exported HTML page
 (use-package htmlize
-  :ensure t
   :hook (org-mode-hook html-mode-hook)
   :defer t)
 
 ;; impatient-mode --- see the effect of HTML as you type it
 (use-package impatient-mode
-  :ensure t
   :hook (html-mode-hook)
   :defer t)
+
+
+;; web-mode --- an autonomous emacs major-mode for editing web templates.
+(add-to-list 'load-path (concat my-emacs-directory "private/"))
+(require 'web-mode)
+(add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.[agj]sp\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
+
+(defun my-web-mode-hook ()
+  "Hooks for Web mode."
+  (setq web-mode-engines-alist
+	'(("django" . "\\.html\\'")
+	  ("php"    . "\\.phtml\\'")
+          ("blade"  . "\\.blade\\.")))
+  (setq web-mode-markup-indent-offset 2)
+  (setq web-mode-css-indent-offset 2)
+  (setq web-mode-code-indent-offset 2)
+  (setq web-mode-style-padding 1)
+  (setq web-mode-script-padding 1)
+  (setq web-mode-block-padding 0)
+  (setq web-mode-comment-style 2)
+)
+(add-hook 'web-mode-hook  'my-web-mode-hook)
 
 (provide 'core-html)
 ;;; core-html.el ends here
