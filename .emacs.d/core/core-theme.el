1314cd02cbd0c2df37fc959065ffc5970ded69a6
Arion73 <24527962+Arion73@users.noreply.github.com>
2019-04-05 11:30:50 +0200

    add new function (set background transparent)

--- .emacs.d/core/core-theme.el
+++ .emacs.d/core/core-theme.el
@@ -24,5 +24,18 @@
   (load-theme 'zelin-dark-02-terminal t))
 
 
+;; set background to be transparent
+(global-set-key (kbd "C-c C-t") 'loop-alpha)  ;; short-cut key
+(setq alpha-list '((85 55) (100 100)))
+(defun loop-alpha ()
+  (interactive)
+  (let ((h (car alpha-list)))
+    ((lambda (a ab)
+       (set-frame-parameter (selected-frame) 'alpha (list a ab))
+       (add-to-list 'default-frame-alist (cons 'alpha (list a ab)))
+       ) (car h) (car (cdr h)))
+    (setq alpha-list (cdr (append alpha-list (list h))))))
+
+
 (provide 'core-theme)
 ;;; core-theme.el ends here
